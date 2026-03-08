0072094a0b404f42275b089295088720fbc5b5e1
Zea <Zea@LaptopDeZea>
2026-03-08 17:28:22 -0600

    Cambios pom y demoapplications

--- src/test/java/com/DesarrolloWeb/demo/DemoApplicationTests.java
+++ src/test/java/com/DesarrolloWeb/demo/DemoApplicationTests.java
@@ -1,23 +1,14 @@
 package com.DesarrolloWeb.demo;
 
 import org.junit.jupiter.api.Test;
-<<<<<<< HEAD
-
-class DemoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-}
-=======
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
->>>>>>> f47ed51836338af13f4154e584401cc10cc62a97
+
