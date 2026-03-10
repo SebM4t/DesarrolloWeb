14895b6a75555dd5b62fdcc72e18aa2386d26829
Zea <Zea@LaptopDeZea>
2026-03-10 13:46:56 -0600

    Login y Registro con boobstrap funcionales

--- src/main/java/com/DesarrolloWeb/demo/controller/CuentaController.java
+++ src/main/java/com/DesarrolloWeb/demo/controller/CuentaController.java
@@ -1,26 +1,52 @@
 package com.DesarrolloWeb.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class CuentaController {
- @GetMapping("/cuenta")
-    public String mostrarCuenta() {
-        return "cuenta/cuenta";
+  
+    @GetMapping("/cuenta")
+    public String mostrarLogin(Model model) {
+       
+        return "cuenta/listado"; 
     }
 
+ 
+    @PostMapping("/login")
+    public String procesarLogin(@RequestParam String usuario,
+                                @RequestParam String password,
+                                Model model) {
+
+       
+            return "cuenta/listado"; 
+        }
+    
+
+   
     @GetMapping("/registro")
-    public String mostrarRegistro() {
-        return "cuenta/registro";
+    public String mostrarRegistro(Model model) {
+        return "cuenta/detalle"; 
     }
 
+    
     @PostMapping("/registro")
-    public String registrarUsuario() {
-        return "redirect:/cuenta";
-    }    
+    public String registrarUsuario(@RequestParam String nombreCompleto,
+                                   @RequestParam String telefono,
+                                   @RequestParam String correo,
+                                   @RequestParam String usuario,
+                                   @RequestParam String password,
+                                   Model model) {
+
+        
+
+        return "redirect:/cuenta"; 
+    }
 }
+
 
 
